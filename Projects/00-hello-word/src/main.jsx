3d2c05616e1bf5d669187ef4fbb12e569709b881
amaiagalvez <amaiagalvez@hotmail.com>
2023-01-21 12:50:38 +0100

    comentarios

--- Projects/00-hello-word/src/main.jsx
+++ Projects/00-hello-word/src/main.jsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// Este es el punto de partida de la aplicación
+// root es el id del div que está en el index.html, donde vamos a renderizar nuestra aplicación
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App /> 
   </React.StrictMode>,
 )
